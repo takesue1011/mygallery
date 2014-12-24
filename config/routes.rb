f9df8d18032f1e008b16570c019729539e53c2e2
Kenjiro Takesue <takesue1011@Kenjiro-no-MacBook-Pro.local>
2014-12-24 18:21:50 +0900

    CSSの微調整を行った。

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users                        # 会員登録・ログイン・ログアウト
   root 'photos#index'                      # ルートパスの設定
   get 'photos'      => 'photos#index'      # マイページ(写真一覧画面)（photos: コントローラー名、index: アクション名）
   get 'photos/new'  => 'photos#new'        # 写真投稿ページ
   post 'photos'     => 'photos#create'     # 写真投稿機能
   get "attachments/show"
-  # 会員登録ページ
-  # ログインページ
   # トップページ
 
 end
