47b63d3c5a7cf79c1690be66548c18f75a91061d
Kenjiro Takesue <takesue1011@Kenjiro-no-MacBook-Pro.local>
2014-12-25 15:08:28 +0900

    写真投稿の部分の変更

--- app/controllers/attachments_controller.rb
+++
@@ -1,9 +0,0 @@
-class AttachmentsController < ApplicationController
-
-  def show
-    full_path = "#{Rails.root}" + params[:path]
-    image = File.binread(full_path)
-    send_data image, :disposition => 'inline'
-  end
-
-end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,9 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all.order("created_at DESC")
   end
+
   def new
+    @photo = Photo.new
   end
 
   def create
@@ -14,7 +16,7 @@ class PhotosController < ApplicationController
 
   private
   def create_params
-    params.permit(:title, :photographer, :caption, :image)
+    params.require(:photo).permit(:title, :photographer, :caption, :image)
   end
 
   def set_something
